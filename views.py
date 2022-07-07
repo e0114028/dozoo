b4a54d6d4b2c9dce3ca2d559ea246ce3d8a46eb3
金谷和歩 <k.kazuho@warenoMBPja.local>
2022-07-07 09:13:16 +0900

    test

---
+++ views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+
+
+def index(request):
+
+    context = {'latest_question_list': 'latest_question_list'}
+    return render(request, 'dozoo/index.html', context)
